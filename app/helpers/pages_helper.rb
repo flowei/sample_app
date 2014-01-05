029543a862f57696b3930be0bbcf3e846b7962eb
flowe <flowe39@gmail.com>
2014-01-05 04:39:35 +0100

    fin 4.1

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  
+  # Retourne un titre basé sur la page
+  def title
+    title_base = "Simple App du Tutoriel Ruby on Rails"
+    if @title.nil?
+      title_base
+    else
+      "#{title_base} | #{@title}"
+    end
+  end
+  
 end
